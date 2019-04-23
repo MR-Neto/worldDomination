d8a38fe50b2dad58eb02578e6ae33e2ff521977e
Miguel Neto <miguelribeironeto@gmail.com>
2019-04-23 16:15:20 +0200

    Created Function to Load Map

--- monsters/worldDomination.js
+++ monsters/worldDomination.js
@@ -1,25 +1,30 @@
 "use strict";
-
 const fs = require('fs');
-
-const data = fs.readFileSync('./monsters/world_map_small.txt').toString('utf-8').split('\n');
 
 // Map object will store all cities as keys which have an object with  directions(North, South, West, East) as properties. 
 let map = {};
+
+// Array of monsters, which are objects with the properties (name, location, moves and alive)
 let monsters = [];
 
-data.forEach(line => {
-  const city = line.split(' ')[0];
-  const connections = line.split(' ');
-  connections.shift();
-  map[city] = {};
 
-  connections.forEach((connection) => {
-    const direction = connection.split('=')[0];
-    const connectedCity = connection.split('=')[1];
-    map[city][direction] = connectedCity;
-  })
-});
+function loadMap(){
+  const data = fs.readFileSync('./monsters/world_map_small.txt').toString('utf-8').split('\n');
+  
+  data.forEach(line => {
+    const city = line.split(' ')[0];
+    const connections = line.split(' ');
+    connections.shift();
+    map[city] = {};
+  
+    connections.forEach((connection) => {
+      const direction = connection.split('=')[0];
+      const connectedCity = connection.split('=')[1];
+      map[city][direction] = connectedCity;
+    })
+  });
+
+}
 
 function spreadMonsters(numberOfMonsters) {
   const cities = Object.keys(map);
@@ -96,5 +101,6 @@ function logCitiesDestroyed(destroyedCities, killedMonsters) {
 
 }
 
+loadMap();
 spreadMonsters(20);
 checkDestroyedCities(monsters);
